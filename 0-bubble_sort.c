760e1edc462e887fd01982dc115a0058ff10f173
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-24 16:15:55 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,21 +1,35 @@
 #include "sort.h"
+/**
+swap - swaps two input integer vals
+@xp: First int parameter
+@yp: First int parameter
+**/
 
-void swap(int *xp, int *yp) {
+void swap(int *xp, int *yp)
+{
 	int temp = *xp;
 	*xp = *yp;
 	*yp = temp;
 }
+/**
+bubble_sort - function to bubble sort
 
-void bubble_sort(int *array, size_t size) {
+@array: array to be sorted
+@size: size of array
+**/
+void bubble_sort(int *array, size_t size)
+{
 	int i, j;
-	for (i = 0; i < size - 1; i++) {
-		for (j = 0; j < size - i - 1; j++) {
-			if (array[j] > array[j + 1]) {
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
 				swap(&array[j], &array[j + 1]);
 				/** Print the array after each swap **/
-				for (int k = 0; k < size; k++) {
+				for (int k = 0; k < size; k++)
 					printf("%d ", array[k]);
-				}
 				printf("\n");
 			}
 		}
