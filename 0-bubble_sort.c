7fa234d4a149506e1d15c88c904203f89ba4581f
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-24 15:57:13 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+#include "sort.h"
 
 void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
 
 void bubble_sort(int *array, size_t size) {
-    int i, j;
-    for (i = 0; i < size - 1; i++) {
-        for (j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
-                // Print the array after each swap
-                for (int k = 0; k < size; k++) {
-                    printf("%d ", array[k]);
-                }
-                printf("\n");
-            }
-        }
-    }
+	int i, j;
+	for (i = 0; i < size - 1; i++) {
+		for (j = 0; j < size - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				swap(&array[j], &array[j + 1]);
+				/** Print the array after each swap **/
+				for (int k = 0; k < size; k++) {
+					printf("%d ", array[k]);
+				}
+				printf("\n");
+			}
+		}
+	}
 }
