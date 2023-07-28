00de497a8cd8ff67443e8cb6e128e8e7ac16df9a
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-28 21:56:35 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,20 +1,15 @@
 #include "sort.h"
 
-/**
- * counting_sort - function to sort an array of integers in ascending order
- * using the Counting sort algorithm
- *
- * @array: array to be sorted
- * @size: size of the array
- */
 void counting_sort(int *array, size_t size)
 {
     if (array == NULL || size < 2)
         return;
 
-    /* Find the maximum value in the array to determine the counting array size */
     int max_value = array[0];
-    for (size_t i = 1; i < size; i++)
+    size_t i, j;
+
+    /* Find the maximum value in the array to determine the counting array size */
+    for (i = 1; i < size; i++)
     {
         if (array[i] > max_value)
             max_value = array[i];
@@ -29,17 +24,17 @@ void counting_sort(int *array, size_t size)
     }
 
     /* Populate the counting array by counting occurrences of each element */
-    for (size_t i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
         counting_array[array[i]]++;
 
     /* Print the counting array */
     printf("Counting array: ");
-    for (int i = 0; i <= max_value; i++)
+    for (i = 0; i <= max_value; i++)
         printf("%d, ", counting_array[i]);
     printf("\n");
 
     /* Modify the counting array to store the correct positions of each element */
-    for (int i = 1; i <= max_value; i++)
+    for (i = 1; i <= max_value; i++)
         counting_array[i] += counting_array[i - 1];
 
     /* Create a temporary output array to store the sorted elements */
@@ -52,14 +47,14 @@ void counting_sort(int *array, size_t size)
     }
 
     /* Populate the output array using the counting array */
-    for (int i = size - 1; i >= 0; i--)
+    for (i = size - 1; i < size; i--)
     {
         output_array[counting_array[array[i]] - 1] = array[i];
         counting_array[array[i]]--;
     }
 
     /* Copy the sorted output back to the original array */
-    for (size_t i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
         array[i] = output_array[i];
 
     /* Free the allocated memory */
