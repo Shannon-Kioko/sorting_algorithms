d37f8c93b8ac91a66c05536c938492fa5a1ce07e
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-28 21:14:12 +0300

    Add 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * swap - swaps two input integer values
+ * @xp: First int parameter
+ * @yp: First int parameter
+ */
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+/**
+ * selection_sort - function to sort an array of integers in ascending order
+ * using the Selection sort algorithm
+ *
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j, min_idx;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+
+        if (min_idx != i)
+        {
+            swap(&array[i], &array[min_idx]);
+            /** Print the array after each swap **/
+            for (j = 0; j < size; j++)
+                printf("%d ", array[j]);
+            printf("\n");
+        }
+    }
+}
