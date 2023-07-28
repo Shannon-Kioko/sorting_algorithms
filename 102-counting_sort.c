43430930cd7b20a054a9905622bd35cbb86be79f
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-29 02:24:00 +0300

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,25 +1,5 @@
 #include "sort.h"
 
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
 
 void counting_sort(int *array, size_t size)
 {
