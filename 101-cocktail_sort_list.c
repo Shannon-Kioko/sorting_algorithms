1ae42aeffa108e12da479bb1ec1f6e08737d6ec4
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-29 17:00:40 +0300

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,68 +1,98 @@
 #include "sort.h"
 
+void node_swap_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void node_swap_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+
 /**
- * get_max - Get the maximum value in an array of integers.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Return: The maximum integer in the array.
+ * node_swap_ahead - Swap a node in a listint_t doubly-linked list
+ *                   list of integers with the node ahead of it.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ * @tail: A pointer to the tail of the doubly-linked list.
+ * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
  */
-int get_max(int *array, int size)
+void node_swap_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 {
-	int maxi, i;
+	listint_t *tempo = (*shaker)->next;
 
-	for (maxi = array[0], i = 1; i < size; i++)
-	{
-		if (array[i] > maxi)
-			maxi = array[i];
-	}
-
-	return (maxi);
+	if ((*shaker)->prev != NULL)
+		(*shaker)->prev->next = tempo;
+	else
+		*list = tempo;
+	tempo->prev = (*shaker)->prev;
+	(*shaker)->next = tempo->next;
+	if (tempo->next != NULL)
+		tempo->next->prev = *shaker;
+	else
+		*tail = *shaker;
+	(*shaker)->prev = tempo;
+	tempo->next = *shaker;
+	*shaker = tempo;
 }
 
 /**
- * counting_sort - Sort an array of integers in ascending order
- *                 using the counting sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Description: Prints the counting array after setting it up.
+ * node_swap_behind - Swap a node in a listint_t doubly-linked
+ *                    list of integers with the node behind it.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ * @tail: A pointer to the tail of the doubly-linked list.
+ * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
  */
-void counting_sort(int *array, size_t size)
+void node_swap_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 {
-	int *counts, *sorted, maxi, i;
+	listint_t *tempo = (*shaker)->prev;
 
-	if (array == NULL || size < 2)
+	if ((*shaker)->next != NULL)
+		(*shaker)->next->prev = tempo;
+	else
+		*tail = tempo;
+	tempo->next = (*shaker)->next;
+	(*shaker)->prev = tempo->prev;
+	if (tempo->prev != NULL)
+		tempo->prev->next = *shaker;
+	else
+		*list = *shaker;
+	(*shaker)->next = tempo;
+	tempo->prev = *shaker;
+	*shaker = tempo;
+}
+
+/**
+ * cocktail_sort_list - Sort a listint_t doubly-linked list of integers in
+ *                      ascending order using the cocktail shaker algorithm.
+ * @list: A pointer to the head of a listint_t doubly-linked list.
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *tail, *shaker;
+	bool shaken_not_stirred = false;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	sorted = malloc(sizeof(int) * size);
-	if (sorted == NULL)
-		return;
-	maxi = get_max(array, size);
-	counts = malloc(sizeof(int) * (maxi + 1));
-	if (counts == NULL)
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
+
+	while (shaken_not_stirred == false)
 	{
-		free(sorted);
-		return;
+		shaken_not_stirred = true;
+		for (shaker = *list; shaker != tail; shaker = shaker->next)
+		{
+			if (shaker->n > shaker->next->n)
+			{
+				node_swap_ahead(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
+		for (shaker = shaker->prev; shaker != *list;
+				shaker = shaker->prev)
+		{
+			if (shaker->n < shaker->prev->n)
+			{
+				node_swap_behind(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
+				shaken_not_stirred = false;
+			}
+		}
 	}
-
-	for (i = 0; i < (maxi + 1); i++)
-		counts[i] = 0;
-	for (i = 0; i < (int)size; i++)
-		counts[array[i]] += 1;
-	for (i = 0; i < (maxi + 1); i++)
-		counts[i] += counts[i - 1];
-	print_array(counts, maxi + 1);
-
-	for (i = 0; i < (int)size; i++)
-	{
-		sorted[counts[array[i]] - 1] = array[i];
-		counts[array[i]] -= 1;
-	}
-
-	for (i = 0; i < (int)size; i++)
-		array[i] = sorted[i];
-
-	free(sorted);
-	free(counts);
 }
