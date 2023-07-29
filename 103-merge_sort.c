9ba2284a90106a4f9eea0be47ca5a8086eca813c
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2023-07-29 17:11:57 +0300

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -2,63 +2,63 @@
 
 /**
  * merge - Merges two subarrays into one sorted array.
- * @array: Pointer to the original array.
- * @left: Pointer to the left subarray.
- * @left_size: Size of the left subarray.
- * @right: Pointer to the right subarray.
- * @right_size: Size of the right subarray.
+ * @array: Pointer to original array.
+ * @left: Pointer to left subarray.
+ * @sizo_left: Size of the left subarray.
+ * @right: Pointer to right subarray.
+ * @sizo_right: Size of right subarray.
  *
  * Return: Nothing.
  */
-void merge(int *array, int *left, size_t left_size, int *right, size_t right_size)
+void merge(int *array, int *left, size_t sizo_left, int *right, size_t sizo_right)
 {
 	size_t i = 0, j = 0, k = 0;
-	int *temp = malloc((left_size + right_size) * sizeof(int));
+	int *tempo = malloc((sizo_left + sizo_right) * sizeof(int));
 
-	if (temp == NULL)
+	if (tempo == NULL)
 		return;
 
 	printf("Merging...\n[left]: ");
-	print_array(left, left_size);
+	print_array(left, sizo_left);
 	printf("[right]: ");
-	print_array(right, right_size);
+	print_array(right, sizo_right);
 
-	while (i < left_size && j < right_size)
+	while (i < sizo_left && j < sizo_right)
 	{
 		if (left[i] <= right[j])
 		{
-			temp[k] = left[i];
+			tempo[k] = left[i];
 			i++;
 		}
 		else
 		{
-			temp[k] = right[j];
+			tempo[k] = right[j];
 			j++;
 		}
 		k++;
 	}
 
-	while (i < left_size)
+	while (i < sizo_left)
 	{
-		temp[k] = left[i];
+		tempo[k] = left[i];
 		i++;
 		k++;
 	}
 
-	while (j < right_size)
+	while (j < sizo_right)
 	{
-		temp[k] = right[j];
+		tempo[k] = right[j];
 		j++;
 		k++;
 	}
 
-	for (i = 0; i < left_size + right_size; i++)
-		array[i] = temp[i];
+	for (i = 0; i < sizo_left + sizo_right; i++)
+		array[i] = tempo[i];
 
 	printf("[Done]: ");
-	print_array(array, left_size + right_size);
+	print_array(array, sizo_left + sizo_right);
 
-	free(temp);
+	free(tempo);
 }
 
 /**
@@ -73,14 +73,14 @@ void merge_sort(int *array, size_t size)
 {
 	if (size > 1)
 	{
-		size_t mid = size / 2;
-		size_t left_size = mid;
-		size_t right_size = size - mid;
+		size_t midd = size / 2;
+		size_t sizo_left = midd;
+		size_t sizo_right = size - midd;
 		int *left = array;
-		int *right = array + mid;
+		int *right = array + midd;
 
-		merge_sort(left, left_size);
-		merge_sort(right, right_size);
-		merge(array, left, left_size, right, right_size);
+		merge_sort(left, sizo_left);
+		merge_sort(right, sizo_right);
+		merge(array, left, sizo_left, right, sizo_right);
 	}
 }
